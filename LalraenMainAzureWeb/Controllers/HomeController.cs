83e67cdaf4321762814a4247df7f83ed56e0e2dd
Владимир Миненко <minenko.od@gmail.com>
2020-07-22 16:11:42 +0300

    start

---
+++ LalraenMainAzureWeb/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+
+namespace LalraenMainAzureWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult LoginForm()
+        {
+            return View();
+        }
+
+        public ViewResult SignupForm()
+        {
+            return View();
+        }
+
+        public ActionResult Main()
+        {
+            return View();
+        }
+    }
+}
