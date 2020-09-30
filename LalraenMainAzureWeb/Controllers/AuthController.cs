4e59c9ff7638a95331a079fefdc90033fc7f8e9a
Владимир Миненко <minenko.od@gmail.com>
2020-09-30 12:07:03 +0300

    controllers and new views

---
+++ LalraenMainAzureWeb/Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LalraenMainAzureWeb.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        public ActionResult AuthMain()
+        {
+            return View();
+        }
+    }
+}
---
+++ LalraenMainAzureWeb/Controllers/ListsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LalraenMainAzureWeb.Controllers
+{
+    public class ListsController : Controller
+    {
+        // GET: Lists
+        public ActionResult Lists()
+        {
+            return View();
+        }
+    }
+}
